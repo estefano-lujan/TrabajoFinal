3e92ec0061190ce9a045ca4780ff2db0780dbbc3
Estefano-lujan <lujan.estefano@hotmail.com>
2021-08-08 17:50:50 -0300

    usuario y contraseña

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Portal-de-Empleo';
+miFormulario: FormGroup
+constructor( private _builder: FormBuilder){
+this.miFormulario = this._builder.group({
+userEmail: ['', Validators.compose([Validators.email,
+Validators.required])],
+userPassword: ['', Validators.required]
+});
 }
+enviar(values: any){
+console.log(values)
+}
+}
+
+
